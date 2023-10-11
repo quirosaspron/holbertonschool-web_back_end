97178e2d196381e524dba64a85c818cda6f5ab6f
Mateo <mateoquirosaspron@gmail.com>
2023-10-10 19:49:33 -0600

    first task

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''Using variable annotations to add numbers'''
 
-def add(a:float, b:float) -> float:
-	return a + b
+
+def add(a: float, b: float) -> float:
+    return a + b
