53e024473d8ae27dea68696366ac725f577ad7fb
Mateo <mateoquirosaspron@gmail.com>
2023-10-11 20:51:48 -0600

    task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,5 +2,7 @@
 """Takes a list of floats and returns their sum"""
 from typing import List
 
+
 def sum_list(lista: List[float]) -> float:
-	return sum(lista)
+    """Takes the list and outputs the sum"""
+    return sum(lista)
