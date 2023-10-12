36ccfb2b76bb2c4b2448532f35c1d9de66ea2626
Mateo <mateoquirosaspron@gmail.com>
2023-10-11 20:58:54 -0600

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Takes a list of numbers and sums them"""
+from typing import List, Union
+
+
+def sum_list(lista: List[Union[int, float]]) -> float:
+    """Takes the list and outputs the sum"""
+    return float(sum(lista))
