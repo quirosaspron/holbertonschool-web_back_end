82fc363a8d79f08fab86c1894d57e13def1c41d9
Mateo <mateoquirosaspron@gmail.com>
2023-10-12 21:01:32 -0600

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Turns a str and a num and makes them a tuple"""
-from typing import Union
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Takes a str and num, outputs tuple"""
     return (k, float(v*v))
