16227125c5e2bf54c57cea2091c85f8de77c3e80
Mateo <mateoquirosaspron@gmail.com>
2023-10-15 13:22:01 -0600

    task 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> int:
+async def wait_random(max_delay: int = 10) -> float:
     """Generates a random number waits and returns max_delay"""
     random_number = random.uniform(0.0, upper_bound)
     await asyncio.sleep(random_number)
-    return max_delay
+    return random_number
